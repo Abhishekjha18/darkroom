5c45116d9d7e5019633fb295cf5b5f3a9973966c
Abhishek Jha <abhishek22023@iiitd.ac.in>
2026-08-30 23:06:49 +0530

    add: HTTP mime type table

---
+++ src/http/mime.rs
@@ -0,0 +1,7 @@
+//! Content types for the handful of things darkroom serves.
+
+pub const HTML: &str = "text/html; charset=utf-8";
+pub const CSS: &str = "text/css; charset=utf-8";
+pub const JS: &str = "text/javascript; charset=utf-8";
+pub const JSON: &str = "application/json; charset=utf-8";
+pub const TEXT: &str = "text/plain; charset=utf-8";
